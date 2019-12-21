491d714757c459f690318b91f601b214a6575576
John <956407921@qq.com>
2019-12-21 23:34:21 +0800

    "tab切换"

---
+++ src/js/tabs.js
@@ -0,0 +1,23 @@
+{
+	let view = {
+		el: "#tabs"
+	}
+	let model = {}
+	let controller= {
+		init(view,model){
+			this.view = view
+			this.model = model
+			this.bindEvents()
+		},
+		bindEvents(){
+			$(this.view.el).on("click",".tabs-nav > li",(e)=>{
+				let $li = $(e.currentTarget)
+				let tabName = $li.attr("data-tab-name")
+				$li.addClass("active")
+				.siblings().removeClass("active")
+				window.eventHub.emit("selectTab",tabName)
+			})
+		}
+	}
+	controller.init(view,model)
+}
